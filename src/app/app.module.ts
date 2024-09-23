46c45b9528c2ba1d0abf99240eaa2604efa66211
Sebastian Rios Rico <sebastianr@ikbo.co>
2024-09-22 21:30:30 -0500

    Changes in animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './portfolio/page/page.component';
 import { ComponentsModule } from './portfolio/components/components.module';
@@ -13,7 +12,6 @@ import { ComponentsModule } from './portfolio/components/components.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ComponentsModule
   ],
   providers: [],

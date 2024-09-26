1b54360cc22322bd06c05ac2eabee9a8232ec360
Sebastian Rios Rico <sebastianr@ikbo.co>
2024-09-26 18:07:06 -0500

    Adding Skeleton

--- src/app/portfolio/page/page.component.ts
+++ src/app/portfolio/page/page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-page',
@@ -16,12 +15,8 @@ export class PageComponent implements OnInit {
   }
 
   preloadBackgroundImage(): void {
-    const img = new Image();
-    img.src = '../../../assets/background-image-2.jpg';
-    img.onload = () => {
-      setTimeout(() => {
-        this.isImageLoaded = true;
-      }, 1000);
-    };
+    setTimeout(() => {
+      this.isImageLoaded = true;
+    }, 2000);
   }
 }
